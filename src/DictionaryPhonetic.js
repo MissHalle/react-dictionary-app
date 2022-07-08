693cafa93c43820c9be4960b8c6c2583d8ce2648
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-07-08 14:46:10 -0500

    tweaked css file
    requesting 2 apis when keyword changes,
    created and displaying photos

--- src/DictionaryPhonetic.js
+++ src/DictionaryPhonetic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function DictionaryPhonetic(props) {
   return (
     <div className="DictionaryPhonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
         Listen
       </a>
       <br />
--- src/Search.js
+++ src/Search.js
@@ -1,38 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
 import DictionaryResults from "./DictionaryResults";
+import Photos from "./Photos";
 import "./Search.css";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f9170000100000116d69717d47747f68cede5b2ee6b9b8e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        header: headers,
+      })
+      .then(handlePexelsResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Search">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Type a word here"
-          autoFocus={true}
-          onChange={handleKeywordChange}
-        />
-        <input className="Submit" type="submit" value="Search" />
-      </form>
-      <DictionaryResults results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Search">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Type a word here"
+            autoFocus={true}
+            onChange={handleKeywordChange}
+          />
+          <input className="Submit" type="submit" value="Search" />
+        </form>
+        <DictionaryResults results={results} />
+        <hr />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading..";
+  }
 }
